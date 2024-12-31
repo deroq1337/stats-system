2485ef652ea106e7838c49d25fb577b4933f8a41
lukas2o11 <miles051104@gmail.com>
2025-01-01 00:25:17 +0100

    refactor: made some classes to records

--- src/main/java/com/github/deroq1337/stats/data/models/top10/TopTenList.java
+++ src/main/java/com/github/deroq1337/stats/data/models/top10/TopTenList.java
@@ -1,27 +1,16 @@
 package com.github.deroq1337.stats.data.models.top10;
 
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.RequiredArgsConstructor;
-import lombok.ToString;
 import org.bukkit.command.CommandSender;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.Set;
 
-@RequiredArgsConstructor
-@Getter
-@ToString
-@EqualsAndHashCode
-public class TopTenList {
-
-    private final int interval;
-    private final @NotNull Set<TopTenListEntry> entries;
+public record TopTenList(int interval, @NotNull Set<TopTenListEntry> entries){
 
     public void print(@NotNull CommandSender commandSender) {
         StringBuilder builder = new StringBuilder("Top 10 (" + interval + "d)\n");
-        entries.forEach(entry -> builder.append("#").append(entry.getRank()).append(": ")
-                .append(entry.getPlayer().toString()).append(" - ").append(entry.getValue()).append(" Kills"));
+        entries.forEach(entry -> builder.append("#").append(entry.rank()).append(": ")
+                .append(entry.player()).append(" - ").append(entry.value()).append(" Kills"));
         commandSender.sendMessage(builder.toString());
     }
 }
--- src/main/java/com/github/deroq1337/stats/data/models/top10/TopTenListEntry.java
+++ src/main/java/com/github/deroq1337/stats/data/models/top10/TopTenListEntry.java
@@ -1,17 +1,8 @@
 package com.github.deroq1337.stats.data.models.top10;
 
-import lombok.*;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.UUID;
 
-@RequiredArgsConstructor
-@Getter
-@ToString
-@EqualsAndHashCode
-public class TopTenListEntry {
-
-    private final @NotNull UUID player;
-    private final int rank;
-    private final long value;
+public record TopTenListEntry(@NotNull UUID player, int rank, long value) {
 }
